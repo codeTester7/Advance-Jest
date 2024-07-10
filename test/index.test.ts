de5c3ea0b925f0745e318d4acf162f46df7118d3
codeTester7 <testerqualityengineer7@gmail.com>
2024-07-10 15:18:44 +0530

    new files added

--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import Requests from '../controller/index.controller';
-// const config =  require('../configs/base.config');
-// const request = require(config.baseURL);
-describe('GET Request', () => {
-    test('GET /posts', async () => {
-        const res = await Requests.getPosts();
-        console.log(res.body);
-    })
-})
